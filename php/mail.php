57eb830b5ffad94b99f5e8da83de4124703e3e6e
Eyouel Ayalew <eyouelaya@gmail.com>
2018-03-14 18:06:18 +0300

    fixed submit

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,7 @@
 <?php
     $name = $_GET['name'];
-    $email = $_GET['email']; 
-    
+    $email = $_GET['email'];
+    $msg = "Name ".$name." Email ".$email; 
+    //replace with own email
+    mail("eyouelaya@belmondocorp.com","email Notification","$msg");
 ?>

3cf0d423c87d91bccf86a41f56745ff08781c15e
Eyouel <eyouelaya@gmail.com>
2018-03-14 15:49:21 +0300

    added expired time

--- php/mail.php
+++ php/mail.php
@@ -1,6 +1,5 @@
 <?php
-    // $email = $_GET['email']; 
-    // mail("eyouel@belmondocorp.tech","Notfication Request", $email);
-    // header("..index.html");
-    echo "this";
+    $email = $_GET['email']; 
+    mail("eyouel@belmondocorp.tech","Notfication Request", $email);
+    header("..index.html"); 
 ?>

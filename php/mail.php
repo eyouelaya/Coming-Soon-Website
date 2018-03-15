8bce728239f26064262016dd1259a258ba84f3fa
Eyouel <eyouelaya@gmail.com>
2018-03-15 10:17:48 +0300

    header fix

--- php/mail.php
+++ php/mail.php
@@ -3,5 +3,6 @@
     $email = $_GET['email'];
     $msg = "Name ".$name." Email ".$email; 
     //replace with own email
-    mail("eyouelaya@belmondocorp.com","email Notification","$msg");
+    mail("smth@smth.com","email Notification","$msg");
+    header('location: ../index.html');
 ?>
